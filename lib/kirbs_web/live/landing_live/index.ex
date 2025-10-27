4b46b8b496a135a498f08bbc3f021aa34c645ea4
Krister Viirsaar <krister.viirsaar@gmail.com>
2025-10-27 09:49:19 +0200

    delivery address

--- lib/kirbs_web/live/landing_live/index.ex
+++ lib/kirbs_web/live/landing_live/index.ex
@@ -6,6 +6,15 @@ defmodule KirbsWeb.LandingLive.Index do
   def mount(_params, _session, socket) do
     {:ok,
      socket
-     |> assign(:page_title, "Käed Vabad Kirbukas - Lapse riideid müüa ilma vaevata")}
+     |> assign(:page_title, "Käed Vabad Kirbukas - Lapse riideid müüa ilma vaevata")
+     |> assign(:show_modal, nil)}
+  end
+
+  def handle_event("show_modal", %{"type" => type}, socket) do
+    {:noreply, assign(socket, :show_modal, type)}
+  end
+
+  def handle_event("close_modal", _, socket) do
+    {:noreply, assign(socket, :show_modal, nil)}
   end
 end
