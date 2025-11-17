689e415d0d46b811f15fcca285206d5aa667c122
Krister Viirsaar <krister.viirsaar@gmail.com>
2025-11-17 12:30:25 +0200

    fix review error

--- lib/kirbs/resources/item.ex
+++ lib/kirbs/resources/item.ex
@@ -57,12 +57,12 @@ defmodule Kirbs.Resources.Item do
     end
 
     read :get_items_needing_review do
-      prepare build(sort: [{:bag, :created_at}, :created_at], load: :bag)
+      prepare build(sort: [created_at: :asc], load: :bag)
       filter expr(status in [:pending, :ai_processed])
     end
 
     read :get_first_item_needing_review do
-      prepare build(sort: [{:bag, :created_at}, :created_at], limit: 1, load: :bag)
+      prepare build(sort: [created_at: :asc], limit: 1, load: :bag)
       filter expr(status in [:pending, :ai_processed])
     end
 
