26f897cfb385e5eeaaf8517a789e3b916683ac8f
Krister Viirsaar <krister.viirsaar@gmail.com>
2026-01-07 14:16:26 +0200

    can't get rid of rate limit error

--- config/config.exs
+++ config/config.exs
@@ -13,7 +13,7 @@ config :ash_oban, pro?: false
 config :kirbs, Oban,
   engine: Oban.Engines.Basic,
   notifier: Oban.Notifiers.Postgres,
-  queues: [default: 10, ai: 3],
+  queues: [default: 10, ai: 1],
   repo: Kirbs.Repo,
   plugins: [
     {Oban.Plugins.Cron,
